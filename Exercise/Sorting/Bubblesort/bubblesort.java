03d22883a5991d6c98de961174407ae5bf24899b
Allen Douglas <allenissabigboi@gmail.com>
2024-10-19 17:28:38 -0700

    Completed bubble sort and insertion sort no bugs

--- Exercise/Sorting/Bubblesort/bubblesort.java
+++ Exercise/Sorting/Bubblesort/bubblesort.java
@@ -1,9 +1,33 @@
 package Exercise.Sorting.Bubblesort;
 
+import java.util.Arrays;
+
 class bubblesort {
 
-    public static void main(String[] args) {
-        
+    void sort(int[] e) {
+
+        for (int i = 0; i < e.length - 1; i++) {
+            for (int j = 0; j < e.length - i - 1; j++) {
+                if (e[j] > e[j + 1]) {
+                    int temp = e[j];
+                    e[j] = e[j + 1];
+                    e[j + 1 ] = temp;
+
+                }
+            }
+        }
+        System.out.println(Arrays.toString(e));
     }
-    
+
+
+    public static void main(String[] args) {
+
+
+        bubblesort sorting = new bubblesort();
+
+        int[] e = { 3, 34, 54, 7, 65, 4, 21, 4, 4 };
+
+        sorting.sort(e);
+    }
+
 }
