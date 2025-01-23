89712e44bd23c0510f9daeb0acf12347487b652f
Allen Douglas <allenissabigboi@gmail.com>
2025-01-23 15:32:34 -0800

    made changes

--- LEETCODE/cancellation.js
+++ LEETCODE/cancellation.js
@@ -1,13 +1,30 @@
-var cancellable = function(fn, args, t) {
-    return () => setTimeout(() => console.log(fn(...args)), t)
+// var cancellable = function(fn, args, t) {
+//     return () => setTimeout(() => console.log(fn(...args)), t)
 
- };
-
-
-const testFunc = (x) => x * 2;
+//  };
 
 
-const food2 = cancellable(testFunc, [2], 50);
+// const testFunc = (x) => x * 2;
 
-setTimeout(food2, 20)
 
+// const food2 = cancellable(testFunc, [2], 50);
+
+// setTimeout(food2, 20)
+
+
+
+const EmpObj = {
+    name: "Allen",
+    food: "Run"
+}
+const EmpArr = [null, false, 0]
+const isEmpty = (obj) => {
+    for (const index in obj) {
+        if (JSON.stringify(index)) return true
+    }
+    return false
+}
+
+// console.log(isEmpty(EmpObj))
+console.log(isEmpty(EmpArr))
+// console.log(Boolean(EmpObj[0]))
